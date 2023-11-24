76770cbc48578b397a765c4f6e23256dfec41b55
sodiumts <ernests.malnacs@gmail.com>
2023-11-24 22:38:59 +0100

    a

--- discord_client.py
+++ discord_client.py
@@ -3,19 +3,20 @@ import asyncio
 from datetime import datetime
 
 class MainClient(discord.Client):
-    def __init__(self,intents, session):
+    def __init__(self,intents, session, command_tree):
         super().__init__(intents=intents)
         self.synced = False
         self.session = session
         self.rooms_message = None
         self.channel = None
+        self.command_tree = command_tree
         
 
 
     async def on_ready(self):
         await self.wait_until_ready()
         if not self.synced:
-            # await tree.sync() #sync the commands to the server
+            await self.command_tree.sync() #sync the commands to the server
             self.synced = True
 
         self.channel = self.get_channel(978241396714639374)
@@ -32,7 +33,7 @@ class MainClient(discord.Client):
                 await message.delete()
 
             old_content = embed_contents
-            
+
             await self.update_rooms_embed()
             await asyncio.sleep(10*60)
 
